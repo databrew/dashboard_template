2d481b0103582bce3d8c2bad4f5c391c9cc1c711
Joe Brew <joebrew@gmail.com>
2019-05-16 06:56:05 +0545

    sample

---
+++ app_with_db.R
@@ -0,0 +1,175 @@
+library(shiny)
+library(shinydashboard)
+library(tidyverse)
+library(ggplot2)
+
+# Function for checking log-in
+check_password <- function(user, password){
+  message('User/password combination is correct')
+  if(user == 'joe'){
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+  # Replace code above with password validation
+}
+
+# Function for adding new user
+add_user <- function(user, password){
+  message('Account just created with the following credentials')
+  message('---User: ', user)
+  message('---Password: ', password)
+  # Add code here to add user to database
+}
+
+header <- dashboardHeader(title="App title")
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem(
+      text="Main",
+      tabName="main",
+      icon=icon("eye")),
+    menuItem(
+      text = 'About',
+      tabName = 'about',
+      icon = icon("cog", lib = "glyphicon"))
+  )
+)
+
+body <- dashboardBody(
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+  ),
+  tabItems(
+    tabItem(
+      tabName="main",
+      fluidPage(
+        h1('Main page')
+      )
+    ),
+    tabItem(
+      tabName = 'about',
+      fluidPage(
+        fluidRow(
+          div(img(src='logo_clear.png', align = "center"), style="text-align: center;"),
+          h4('Hosted by ',
+             a(href = 'http://databrew.cc',
+               target='_blank', 'Databrew'),
+             align = 'center'),
+          p('Empowering research and analysis through collaborative data science.', align = 'center'),
+          div(a(actionButton(inputId = "email", label = "info@databrew.cc", 
+                             icon = icon("envelope", lib = "font-awesome")),
+                href="mailto:info@databrew.cc",
+                align = 'center')), 
+          style = 'text-align:center;'
+        )
+      )
+    )
+  )
+)
+
+# UI
+ui <- dashboardPage(header, sidebar, body, skin="blue")
+
+# Server
+server <- function(input, output) {
+  
+  # Reactive values
+  logged_in <- reactiveVal(value = FALSE)
+  modal_text <- reactiveVal(value = '')
+  # Log in modal
+  showModal(
+    modalDialog(
+      uiOutput('modal_ui'),
+      footer = NULL
+    )
+  )
+  
+  
+  # See if log-in worked
+  observeEvent(input$submit, {
+    cp <- check_password(user = input$user,
+                         password = input$password)
+    logged_in(cp)
+  })
+  
+  # When OK button is pressed, attempt to log-in. If success,
+  # remove modal.
+  observeEvent(input$submit, {
+    # Did login work?
+    li <- logged_in()
+    if(li){
+      # Update the reactive modal_text
+      modal_text(paste0('Logged in as ', input$user))
+      removeModal()
+    } else {
+      # Update the reactive modal_text
+      modal_text(paste0('That user/password combination is not correct.'))
+    }
+  })
+  
+  # Make a switcher between the log in vs. create account menus
+  create_account <- reactiveVal(FALSE)
+  observeEvent(input$create_account,{
+    currently <- create_account()
+    nowly <- !currently
+    create_account(nowly)
+  })
+  observeEvent(input$submit_create_account,{
+    currently <- create_account()
+    nowly <- !currently
+    create_account(nowly)
+  })
+  observeEvent(input$back,{
+    currently <- create_account()
+    nowly <- !currently
+    create_account(nowly)
+  })
+  
+  output$modal_ui <- renderUI({
+    # Capture the modal text.
+    mt <- modal_text()
+    # See if we're in account creation vs log in mode
+    account_creation <- create_account()
+    if(account_creation){
+      fluidPage(
+        fluidRow(
+          column(12,
+                 align = 'right',
+                 actionButton('back',
+                              'Back'))
+        ),
+        h3(textInput('create_user', 'Create username'),
+           textInput('create_password', 'Create password')),
+        fluidRow(
+          column(12, align = 'right',
+                 actionButton('submit_create_account',
+                              'Create account'))
+        )
+      )
+    } else {
+      fluidPage(
+        h3(textInput('user', 'Username',
+                     value = 'joe'),
+           passwordInput('password', 'Password')),
+        fluidRow(
+          column(6,
+                 actionButton('submit',
+                              'Submit')),
+          column(6, align = 'right',
+                 actionButton('create_account',
+                              'Create account'))
+        ),
+        p(mt)
+      )}
+  })
+  
+  # Observe account creation
+  observeEvent(input$submit_create_account,{
+    add_user(user = input$create_user,
+             password = input$create_password)
+  })
+  
+}
+
+shinyApp(ui, server)
